aa90191d07c7c864dfacad3f28ad8027fc990964
Caroline <carolinesitter24@gmail.com>
2022-04-06 20:22:17 -0500

    Fetch Request Added, profile.js Completed

--- static/profile.js
+++ static/profile.js
@@ -6,14 +6,27 @@ function submitProfile(evt) {
     age: document.querySelector('#age-field').value,
     occupation: document.querySelector('#occupation-field').value,
     // fill in the rest
-
-  
-
-  };
+};
 
   // make request to server to get the data
-  // add the data the server returns to the document
-  // (you can add it to the end of the div with ID "profiles")
+  fetch('/api/profile', {
+    method : 'POST',
+    body : JSON.stringify(data),
+    headers : {
+      'Content-Type' : 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    // add the data the server returns to the document
+    .then(jsonData => {
+      // (you can add it to the end of the div with ID "profiles")
+      document
+        .querySelector('#profiles')
+        .insertAdjacentHTML(
+          'beforeend',
+          `<li>${jsonData.fullname}, ${jsonData.age}, ${jsonData.occupation}</li>`
+        );
+    });
 }
 
 document.querySelector('#profile-form').addEventListener('submit', submitProfile);
